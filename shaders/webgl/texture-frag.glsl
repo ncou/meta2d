70ee48417fa3069307cb7ada5edbb72b4d7edd23
Arturs Šefers <arthur.der.shefer@gmail.com>
2017-02-27 12:14:34 +0200

    Update texture shader.

--- shaders/webgl/texture-frag.glsl
+++ shaders/webgl/texture-frag.glsl
@@ -2,10 +2,10 @@ precision mediump float;
 
 import gamma.glsl
 
-uniform sampler2D map;
+uniform sampler2D albedo;
 
 varying vec2 var_uv;
 
 void main() {
-	gl_FragColor = toGamma(texture2D(map, var_uv))
+	gl_FragColor = toGamma(texture2D(albedo, var_uv))
 }
