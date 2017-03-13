567798d845132b08b831344714fc1ad064fe703e
Arturs Šefers <arthur.der.shefer@gmail.com>
2017-03-13 20:31:03 +0200

    Fix shaders

--- shaders/webgl/skybox-vert.glsl
+++ shaders/webgl/skybox-vert.glsl
@@ -9,7 +9,6 @@ void main()
 {
     vec4 unprojected = matrixInverseProjection * position;
     varNormal = (matrixTransposeView * unprojected).xyz;
-    varNormal.y *= -1.0;
 
     gl_Position = position;
 }
--- shaders/webgl/texture-frag.glsl
+++ shaders/webgl/texture-frag.glsl
@@ -7,5 +7,5 @@ uniform sampler2D albedo;
 varying vec2 var_uv;
 
 void main() {
-	gl_FragColor = toGamma(texture2D(albedo, var_uv))
+	gl_FragColor = toGamma(texture2D(albedo, var_uv));
 }
